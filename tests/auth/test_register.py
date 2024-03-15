3cb06b2c4be5e09f5c0ff853640df0c88a0b423f
vi.vala.vi <sovestyaaaa@gmail.com>
2024-03-16 01:26:16 +0300

    add verif tests

--- tests/auth/test_register.py
+++ tests/auth/test_register.py
@@ -1,6 +1,8 @@
-from httpx import AsyncClient, HTTPStatusError
+from httpx import AsyncClient
 from fastapi import status
 
+from src.exceptions import ObjectNotFoundError
+from src.auth.logic import UserTokenVerify
 from src.auth.models import Role, User
 from src.config import config
 from tests.conftest import (
@@ -10,9 +12,12 @@ from tests.conftest import (
 
 
 class TestRegister:
-    url_register = "api/v1/auth/register"
-    url_login = "api/v1/auth/login"
-    url_users = "api/v1/users"
+    api_version = "api/v1"
+    url_register = f"{api_version}/auth/register"
+    url_request_verify = f"{api_version}/auth/request-verify-token"
+    url_accept = f"{api_version}/auth/accept"
+    url_login = f"{api_version}/auth/login"
+    url_users = f"{api_version}/users"
 
     async def test_register(self, ac: AsyncClient, roles: list[Role]) -> None:
         """
@@ -37,6 +42,53 @@ class TestRegister:
         async with get_async_session_context() as session:
             user = await session.get(User, response.json()["id"])
             assert user
+
+    async def test_verivication_user(
+            self, ac: AsyncClient, user: User
+    ) -> None:
+        """Тестирование верификации пользователя."""
+
+        assert user.is_verified is False
+        # Отправляем несколько запросов, чтобы проверить, что токены заменяются
+        response = await ac.post(
+            self.url_request_verify,
+            json={
+                "email": user.email
+            }
+        )
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        response = await ac.post(
+            self.url_request_verify,
+            json={
+                "email": user.email
+            }
+        )
+        assert response.status_code == status.HTTP_202_ACCEPTED
+
+        # Получаем токен
+        async with get_async_session_context() as session:
+            token = await UserTokenVerify.crud.get(session, 'user_id', user.id)
+            assert token
+
+            # Верифицируемся
+            response = await ac.get(
+                f'{self.url_accept}?token={token.token_verify}',
+            )
+            assert response.status_code == status.HTTP_200_OK
+
+            user = await session.get(User, user.id)
+            assert user.is_verified is True
+            assert user.is_active is True
+            assert user.is_superuser is False
+
+            # Проверяем, что временный токен удалился и больше токенов нет.
+            try:
+                await UserTokenVerify.crud.get(session, 'user_id', user.id)
+                assert False
+            except ObjectNotFoundError:
+                pass
+            except Exception:
+                assert False
 
     async def test_register_already_exists(
             self, ac: AsyncClient, user: User
